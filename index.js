af29b8b58ee195379a08e064d22b220354c64508
Palani C <palani@canva.com>
2021-09-06 13:57:03 +1000

    changed to static dir

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const port = process.env.PORT || 3000;
-
-express()
-  .use(express.static('./static'))
-  .listen(port, () => console.log(`listening.on http://localhost:${port}`));
