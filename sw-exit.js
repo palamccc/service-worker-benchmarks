b36b0302e4a20789d242d5fd1635ad67b0b58b3a
Palani C <palani@canva.com>
2021-09-06 18:34:48 +1000

    added sw-exit.js

---
+++ sw-exit.js
@@ -0,0 +1,17 @@
+const scriptName = 'sw-exit.js';
+const log = (msg) => console.log(`${scriptName}: ${msg}`);
+log('start');
+
+
+self.addEventListener('install', () => {
+  log('install');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  log('activate');
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+});
